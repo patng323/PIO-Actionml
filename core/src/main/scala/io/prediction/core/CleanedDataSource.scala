422d6939268bfb90d4f46bcb7b5131ae1f1197d5
Maxim Korolyov <mk@gojuno.com>
2016-01-08 15:49:19 +0300

    wip-datasource-cleanup-old-events. added compression of LEvents.

--- core/src/main/scala/io/prediction/core/CleanedDataSource.scala
+++ core/src/main/scala/io/prediction/core/CleanedDataSource.scala
@@ -77,35 +77,22 @@ trait CleanedDataSource {
   }
 
   def compressPProperties(sc: SparkContext, rdd: RDD[Event]): RDD[Event] = {
-    rdd.filter(e => e.event == "set" || e.event == "unset")
+    rdd.filter(isSetEvent)
       .groupBy(_.entityType)
       .map { pair =>
         val (_, ls) = pair
-        ls.find(_.event == "set") match {
-
-          case Some(first) =>
-            ls.reduce { (e1, e2) =>
-              val props = e2.event match {
-                case "set" =>
-                  e1.properties.fields ++ e2.properties.fields
-                case "unset" =>
-                  e1.properties.fields
-                    .filterKeys(f => !e2.properties.fields.contains(f))
-              }
-              e1.copy(properties = DataMap(props))
-            }
-
-          case None =>
-            ls.reduce { (e1, e2) =>
-              e1.copy(properties =
-                DataMap(e1.properties.fields ++ e2.properties.fields)
-              )
-            }
-        }
-      }
+        compress(ls)
+      } ++ rdd.filter(!isSetEvent(_))
   }
 
-  def compressLProperties(): Unit = {}
+  def compressLProperties(events: Iterator[Event]): Iterable[Event] = {
+    events.filter(isSetEvent).toIterable
+      .groupBy(_.entityType)
+      .map { pair =>
+        val (_, ls) = pair
+        compress(ls)
+      } ++ events.filter(!isSetEvent(_))
+  }
 
   def removePDuplicates(): Unit = {}
 
@@ -138,31 +125,65 @@ trait CleanedDataSource {
       case None =>
         rdd
     }
+  }
 
-    /** :: DeveloperApi ::
-      *
-      * Filters most recent, compress properties and removes duplicates of LEvents
-      *
-      * @return Iterator[Event] most recent LEvents
-      */
-    @DeveloperApi
-    def cleanedLEvents: Iterator[Event] = {
-      cleanLEvents()
-      getCleanedLEvents()
-    }
+  /** :: DeveloperApi ::
+    *
+    * Filters most recent, compress properties and removes duplicates of LEvents
+    *
+    * @return Iterator[Event] most recent LEvents
+    */
+  @DeveloperApi
+  def cleanedLEvents: Iterable[Event] = {
+    val stage1 = getCleanedLEvents()
+    cleanLEvents(stage1)
+  }
 
-    /** :: DeveloperApi ::
-      *
-      * Filters most recent, compress properties of LEvents
-      */
-    @DeveloperApi
-    def cleanLEvents(): Unit = {
-      eventWindow.map { ew =>
-        if (ew.compressProperties) compressLProperties()
-        if (ew.removeDuplicates) removeLDuplicates()
-      }
+  /** :: DeveloperApi ::
+    *
+    * Filters most recent, compress properties of LEvents
+    */
+  @DeveloperApi
+  def cleanLEvents(ls: Iterator[Event]): Iterable[Event] = {
+    eventWindow match {
+      case Some(ew) =>
+        var updated =
+          if (ew.compressProperties) compressLProperties(ls) else ls.toIterable
+        //if (ew.removeDuplicates) removePDuplicates(updated)
+        updated
+      case None =>
+        ls.toIterable
     }
   }
+
+  private def isSetEvent(e: Event): Boolean = {
+    e.event == "set" || e.event == "unset"
+  }
+
+  private def compress(events: Iterable[Event]): Event = {
+    events.find(_.event == "set") match {
+
+      case Some(first) =>
+        events.reduce { (e1, e2) =>
+          val props = e2.event match {
+            case "set" =>
+              e1.properties.fields ++ e2.properties.fields
+            case "unset" =>
+              e1.properties.fields
+                .filterKeys(f => !e2.properties.fields.contains(f))
+          }
+          e1.copy(properties = DataMap(props))
+        }
+
+      case None =>
+        events.reduce { (e1, e2) =>
+          e1.copy(properties =
+            DataMap(e1.properties.fields ++ e2.properties.fields)
+          )
+        }
+    }
+  }
+}
 
 case class EventWindow(
   duration: Option[Int] = None,
